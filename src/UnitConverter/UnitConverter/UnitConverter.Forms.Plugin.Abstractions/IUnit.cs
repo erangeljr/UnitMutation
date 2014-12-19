035ddd0cb87fd5b0ddab32a2680e13bb00ecb741
erangel <eddie.rangel@doverals.com>
2014-12-19 15:41:28 -0800

    Adding Interfaces

---
+++ src/UnitConverter/UnitConverter/UnitConverter.Forms.Plugin.Abstractions/IUnit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitConverter.Forms.Plugin.Abstractions
+{
+    public interface IUnit<T> where T : IQuantity<T>
+    {
+        IDimension getDimension();
+        IUnit<T> getSystemUnit();
+        //Map<? extends Unit<?>, Integer> getProductUnits();
+
+        bool isCompatible(IUnit<T> that);
+    }
+}

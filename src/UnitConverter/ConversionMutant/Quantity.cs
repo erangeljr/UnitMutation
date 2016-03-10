70344be2d4ab3922901ad0dd28adad440d47e92d
Eddie Rangel <hacker@eddierangel.com>
2016-03-09 22:10:49 -0800

    Create Base Class

---
+++ src/UnitConverter/ConversionMutant/Quantity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversionMutant
+{
+    /// <summary>
+    /// Base Class
+    /// </summary>
+    /// <typeparam name="TAmount"></typeparam>
+    /// <typeparam name="TUnit"></typeparam>
+    public abstract class Quantity<TAmount, TUnit> : IComparable, IComparable<Quantity<TAmount, TUnit>>,
+         IEquatable<Quantity<TAmount, TUnit>>
+        where TAmount : struct, IComparable
+    {
+        public int CompareTo(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CompareTo(Quantity<TAmount, TUnit> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Equals(Quantity<TAmount, TUnit> other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
